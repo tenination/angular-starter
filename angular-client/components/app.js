a822b9c66e3479899fb5a34f9a90168355e217e1
Tim Ninan <tim.ninan@gmail.com>
2017-11-02 14:05:32 -0500

    info now updates based on user inputted date

--- angular-client/components/app.js
+++ angular-client/components/app.js
@@ -1,6 +1,19 @@
 angular.module('app')
 .controller('AppCtrl', function(itemsService, $scope) {
-	this.inputDateValue = '2015-11-01';
+	this.inputDateValue = '2013-09-22';
+	this.handleChange = function() {
+		console.log(this.inputDateValue);
+		if (this.inputDateValue.length === 10) {
+			console.log('length is 10');
+			console.log('itemService being invoked');
+			itemsService.getAll((data) => {
+    		this.items = data;
+    		this.keys = Object.keys(data.near_earth_objects);
+    		this.sample = '2016-03-11';
+  			}, this.inputDateValue);
+
+		}
+	}
   itemsService.getAll((data) => {
     this.items = data;
     this.keys = Object.keys(data.near_earth_objects);
--- angular-client/services/items.js
+++ angular-client/services/items.js
@@ -5,7 +5,7 @@ angular.module('app')
     console.log('WE ARE IN ITEMS.JS', input);
     var dateQuery = `${now.getFullYear()}` + '-'+ `${now.getMonth()+1}`.padStart(2, '0') + '-' +`${now.getDate()}`.padStart(2, '0');
     console.log(dateQuery);
-    //dateQuery = input;
+    dateQuery = input;
     return $http.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateQuery}&end_date=${dateQuery}&api_key=t5Rklsmd6ulGUv9qOX7RDXvGYpH3an33kdKV1nIB`)
     .then(function({data}) {
       if(callback) {
