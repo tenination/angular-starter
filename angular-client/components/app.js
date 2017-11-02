48a9f066f206ef389bce25190219358ba18b22f3
Tim Ninan <tim.ninan@gmail.com>
2017-11-02 14:14:40 -0500

    default input value is now equal to today's date

--- angular-client/components/app.js
+++ angular-client/components/app.js
@@ -1,11 +1,10 @@
 angular.module('app')
 .controller('AppCtrl', function(itemsService, $scope) {
-	this.inputDateValue = '2013-09-22';
+	var now = new Date();
+	var dateQuery = `${now.getFullYear()}` + '-'+ `${now.getMonth()+1}`.padStart(2, '0') + '-' +`${now.getDate()}`.padStart(2, '0');
+	this.inputDateValue = dateQuery;
 	this.handleChange = function() {
-		console.log(this.inputDateValue);
 		if (this.inputDateValue.length === 10) {
-			console.log('length is 10');
-			console.log('itemService being invoked');
 			itemsService.getAll((data) => {
     		this.items = data;
     		this.keys = Object.keys(data.near_earth_objects);
--- angular-client/services/items.js
+++ angular-client/services/items.js
@@ -1,12 +1,7 @@
 angular.module('app')
 .service('itemsService', function($http) {
   this.getAll = function(callback, input) {
-    var now = new Date();
-    console.log('WE ARE IN ITEMS.JS', input);
-    var dateQuery = `${now.getFullYear()}` + '-'+ `${now.getMonth()+1}`.padStart(2, '0') + '-' +`${now.getDate()}`.padStart(2, '0');
-    console.log(dateQuery);
-    dateQuery = input;
-    return $http.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateQuery}&end_date=${dateQuery}&api_key=t5Rklsmd6ulGUv9qOX7RDXvGYpH3an33kdKV1nIB`)
+    return $http.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${input}&end_date=${input}&api_key=t5Rklsmd6ulGUv9qOX7RDXvGYpH3an33kdKV1nIB`)
     .then(function({data}) {
       if(callback) {
         callback(data);
