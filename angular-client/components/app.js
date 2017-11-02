788bacf2190184190a72e10f4e0bd82b7b8b5513
Tim Ninan <tim.ninan@gmail.com>
2017-11-02 10:37:58 -0500

    got service access to user inputted date value

--- angular-client/components/app.js
+++ angular-client/components/app.js
@@ -1,9 +1,11 @@
 angular.module('app')
-.controller('AppCtrl', function(itemsService) {
+.controller('AppCtrl', function(itemsService, $scope) {
+	this.inputDateValue = '2015-11-01';
   itemsService.getAll((data) => {
     this.items = data;
+    
 
-  });
+  }, this.inputDateValue);
 })
 .component('app', {
   bindings: {},
--- angular-client/services/items.js
+++ angular-client/services/items.js
@@ -1,9 +1,12 @@
 angular.module('app')
 .service('itemsService', function($http) {
-  this.getAll = function(callback) {
+  this.getAll = function(callback, input) {
     var now = new Date();
+    console.log('WE ARE IN ITEMS.JS', input);
     var dateQuery = `${now.getFullYear()}` + '-'+ `${now.getMonth()+1}`.padStart(2, '0') + '-' +`${now.getDate()}`.padStart(2, '0');
-    $http.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateQuery}&end_date=${dateQuery}&api_key=t5Rklsmd6ulGUv9qOX7RDXvGYpH3an33kdKV1nIB`)
+    console.log(dateQuery);
+    //dateQuery = input;
+    return $http.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateQuery}&end_date=${dateQuery}&api_key=t5Rklsmd6ulGUv9qOX7RDXvGYpH3an33kdKV1nIB`)
     .then(function({data}) {
       if(callback) {
         callback(data);
